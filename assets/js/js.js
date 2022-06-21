87f0516a925c37ef8ee0a304721140c3e21a4734
HiNT <2001hieunt89@gmail.com>
2022-06-21 15:03:20 +0700

    full

--- assets/js/js.js
+++ assets/js/js.js
@@ -20,4 +20,45 @@ modalconatainer.addEventListener('click',function(event) {
 })
 
 
+// menu
+var header = document.getElementById('header');
+
+var mobileMenu = document.getElementById('menu-mobile-btn');
+
+var headerheight = header.clientHeight;
+ mobileMenu.onclick = function () {
+     var isClosed = header.clientHeight === headerheight;
+    if(isClosed) {
+        header.style.height = "auto";
+     }
+     else {
+        header.style.height = null;
+     }
+    }
+
+
+    // an menu khi dieu huong
+
+    var menuItems = document.querySelectorAll('#header .nav .item-nav a[href*="#"]');
+    console.log(menuItems);
+    for (var i = 0;i < menuItems.length; i++)
+    {
+        var menuItem = menuItems[i];
+        
+       
+         
+        menuItem.onclick = function(event) {
+        var isParentmenu = this.nextElementSibling && menuItem.nextElementSibling.classList.contains('subnav');
+        if (isParentmenu) {
+            event.preventDefault();
+        }
+        else {  
+            header.style.height = null;
+        }
+         
+     }
+    }
+    
+
+
 
